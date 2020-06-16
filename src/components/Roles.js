df6b6d9137d5b2f8acc983cf601e78f08f6e18a8
Christopher Raible <cmraible1@gmail.com>
2020-06-16 00:31:39 -0700

    finishing roles page

--- src/components/Roles.js
+++ src/components/Roles.js
@@ -1,15 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Box, Button, Form, FormField, Heading, Main, Select, TextInput } from 'grommet';
-import { saveUser } from '../actions/actions'
+import { Box, Button, Form, FormField, Heading, List, Main, Select, Text, TextInput } from 'grommet';
+import { Add, Subtract } from 'grommet-icons';
+import { saveUser, addRole, getRoles, deleteRole } from '../actions/actions'
 
 
-const Roles = ({ organization }) => {
+const Roles = ({ organization, roles, addRole, getRoles, deleteRole }) => {
+
+  const [value, setValue] = useState({
+    name: ''
+  });
+
+  useEffect(() => {
+    getRoles(organization.id)
+  }, [organization.id]);
+
+  const renderChildren = (datum, index) => {
+    return (
+      <Box direction="row" justify="between" align="center">
+        <Text>{datum.name}</Text>
+        <Button
+          size="small"
+          icon={<Subtract />}
+          primary
+          color="status-critical"
+          onClick={() => {
+            deleteRole(datum.id)
+          }}
+        />
+      </Box>
+    )
+  }
 
   return (
-    <Main pad="medium">
+    <Main pad="medium" fill="horizontal" >
       <Heading>Roles</Heading>
-      
+      <Box direction="column" gap="large">
+        <Form
+          onSubmit={({value, touch}) => {
+            addRole({
+              organization: organization.id,
+              name: value.name
+            })
+            setValue({name: ''})
+          }}
+          value={value}
+          onChange={ nextValue => setValue(nextValue) }
+        >
+          <Box direction="row" gap="small">
+            <TextInput required name="name" placeholder="New role" />
+            <Button type="submit" size="small" primary icon={<Add />} />
+          </Box>
+        </Form>
+        <List
+          primaryKey="name"
+          data={roles}
+          children={renderChildren}
+        />
+      </Box>
     </Main>
   )
 
@@ -18,13 +66,22 @@ const Roles = ({ organization }) => {
 const mapStateToProps = state => {
   return {
     organization: state.organization,
-    user: state.user
+    user: state.user,
+    roles: state.roles
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    addRole: (role) => {
+      dispatch(addRole(role))
+    },
+    getRoles: (organization_id) => {
+      dispatch(getRoles(organization_id))
+    },
+    deleteRole: (role_id) => {
+      dispatch(deleteRole(role_id))
+    }
   }
 }
 
