8d44bbb888d766d9e85a21595f2a26840b3651db
Christopher Raible <cmraible1@gmail.com>
2021-03-27 16:56:13 -0700

    fixing path to svg logo

--- src/components/AccountPage.js
+++ src/components/AccountPage.js
@@ -14,7 +14,7 @@ const AccountPage = ({ title, hide_title, children, primary_action, secondary_ac
 
       <Box pad={{top: "xlarge", horizontal: "medium"}} width="medium" alignSelf="start" border={{"side": "right"}} fill="vertical">
         <Box height="xsmall" width="xsmall" alignSelf="center">
-          <Image src="routineops-logo.svg" fit="contain" alt="logo-square" />
+          <Image src="/routineops-logo.svg" fit="contain" alt="logo-square" />
         </Box>
         <Heading level={2} size="xsmall" textAlign="center">{ title }</Heading>
           { children }
