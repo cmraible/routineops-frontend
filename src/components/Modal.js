5afeee7ff483776261cab79af1e5c510e0baf957
Christopher Raible <cmraible1@gmail.com>
2021-04-06 18:23:49 -0700

    removing red background color

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,40 +14,40 @@ const Modal = ({children, close, title}) => {
             animation="fadeIn"
             cy-data="modal"
         >
-                    <Box
-                        direction="row"
-                        align="center"
-                        justify="center"
-                        pad="medium"
-                        gap="medium"
-                        fill="horizontal"
-                        round={{size: "small", corner: "top"}}
-                        background="background-contrast"
-                        style={{position: 'relative'}}
-                    >
-                        <Box
-                            style={{position: "absolute", left: "8px"}}
-                            round="small"
-                            hoverIndicator
-                            pad="small"
-                            onClick={close}
-                            data-cy="close-modal"
-                        >
-                            <LinkPrevious />
-                        </Box>
-                        <Heading
-                            margin="none"
-                            textAlign="center"
-                            fill
-                            level={2}
-                            size="small"
-                        >
-                            {title}
-                        </Heading>
-                    </Box>
-                    <Box>
-                     {children}
-                    </Box>
+            <Box
+                direction="row"
+                align="center"
+                justify="center"
+                pad="medium"
+                gap="medium"
+                fill="horizontal"
+                round={{size: "small", corner: "top"}}
+                background="background-contrast"
+                style={{position: 'relative'}}
+            >
+                <Box
+                    style={{position: "absolute", left: "8px"}}
+                    round="small"
+                    hoverIndicator
+                    pad="small"
+                    onClick={close}
+                    data-cy="close-modal"
+                >
+                    <LinkPrevious />
+                </Box>
+                <Heading
+                    margin="none"
+                    textAlign="center"
+                    fill
+                    level={2}
+                    size="small"
+                >
+                    {title}
+                </Heading>
+            </Box>
+            <Box>
+                {children}
+            </Box>
         </Layer>
     )
 }
