f3cc1ce890f3127c2612979f3683fb1dab50b5f1
Chris Raible <chris@routineops.com>
2021-08-29 20:09:57 -0700

    running identify correctly on account switching

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -153,7 +153,7 @@ export const authSlice = createSlice({
         },
         [chooseAccount]: (state, {payload}) => {
             state.loggedInUser = payload;
-            loginUser(state.auth.entities[payload].user)
+            loginUser(state.entities[payload].user)
         },
         [signup.fulfilled]: (state, {payload}) => {
             credentialsAdapter.upsertOne(state, payload);
