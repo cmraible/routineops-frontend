00c143d752424a49c13426a7a5d7f86faac45637
Chris Raible <chris@routineops.com>
2022-07-27 19:49:28 -0700

    removing option to login with google

--- src/app/AppLoggedOut.js
+++ src/app/AppLoggedOut.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 import { selectNumberLoggedInUsers } from '../features/auth/authSlice';
 import Forgot from '../features/auth/Forgot';
 import ForgotReset from '../features/auth/ForgotReset';
 import Login from '../features/auth/Login';
-import LoginOptions from '../features/auth/LoginOptions';
 import Signup from '../features/auth/Signup';
 import SwitchAccounts from '../features/auth/SwitchAccounts';
-import { useSelector } from 'react-redux';
 
 
 const AppLoggedOut = () => {
@@ -17,8 +16,7 @@ const AppLoggedOut = () => {
     // show switch accounts
     return (
       <Switch>
-        <Route exact path="/login" component={LoginOptions} />
-        <Route exact path="/login/email" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/forgot" component={Forgot} />
         <Route path="/" component={SwitchAccounts} />
@@ -32,10 +30,9 @@ const AppLoggedOut = () => {
         <Route exact path="/forgot" component={Forgot} />
         <Route exact path="/reset/:uid/:token" component={ForgotReset} />
         <Route exact path="/invite/:inviteId" component={Signup} />
-        <Route exact path="/login" component={LoginOptions} />
-        <Route exact path="/login/email" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/switchaccounts" component={SwitchAccounts} />
-        <Route path="/" component={LoginOptions} />
+        <Route path="/" component={Login} />
       </Switch>
     )
   }
