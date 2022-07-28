dd2257b153a06666ed4899f0a9676e4671e17bf0
Chris Raible <chris@routineops.com>
2022-07-27 18:51:02 -0700

    Revert "removing login options screen to removeo option to login with google"
    This reverts commit e2bc53324e3889e1ce518019d81edf3c98cf385f.

--- src/app/AppLoggedOut.js
+++ src/app/AppLoggedOut.js
@@ -4,6 +4,7 @@ import { selectNumberLoggedInUsers } from '../features/auth/authSlice';
 import Forgot from '../features/auth/Forgot';
 import ForgotReset from '../features/auth/ForgotReset';
 import Login from '../features/auth/Login';
+import LoginOptions from '../features/auth/LoginOptions';
 import Signup from '../features/auth/Signup';
 import SwitchAccounts from '../features/auth/SwitchAccounts';
 import { useSelector } from 'react-redux';
@@ -16,7 +17,8 @@ const AppLoggedOut = () => {
     // show switch accounts
     return (
       <Switch>
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/login" component={LoginOptions} />
+        <Route exact path="/login/email" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/forgot" component={Forgot} />
         <Route path="/" component={SwitchAccounts} />
@@ -30,9 +32,10 @@ const AppLoggedOut = () => {
         <Route exact path="/forgot" component={Forgot} />
         <Route exact path="/reset/:uid/:token" component={ForgotReset} />
         <Route exact path="/invite/:inviteId" component={Signup} />
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/login" component={LoginOptions} />
+        <Route exact path="/login/email" component={Login} />
         <Route exact path="/switchaccounts" component={SwitchAccounts} />
-        <Route path="/" component={Login} />
+        <Route path="/" component={LoginOptions} />
       </Switch>
     )
   }
